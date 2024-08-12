57ebb6cc3163a8b3a6e01479b67c54ecfd14f112
Lucas Cherkewski <lucas@lucascherkewski.com>
2024-08-12 18:47:09 -0400

    initial commit

---
+++ download-routes.sh
@@ -0,0 +1,5 @@
+curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-15' > weekday.json
+
+curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-13' > saturday.json
+
+curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-14' > sunday.json
