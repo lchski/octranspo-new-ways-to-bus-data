a2a95271a7c66402600e8573dcecd91b16e949f2
Lucas Cherkewski <lucas@lucascherkewski.com>
2024-08-12 18:48:05 -0400

    output data files to data/routes

--- download-routes.sh
+++ download-routes.sh
@@ -1,5 +1,5 @@
-curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-15' > weekday.json
+curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-15' > data/routes/weekday.json
 
-curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-13' > saturday.json
+curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-13' > data/routes/saturday.json
 
-curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-14' > sunday.json
+curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-14' > data/routes/sunday.json
