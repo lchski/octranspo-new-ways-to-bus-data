4af09db42830279ae574434aed99e5cef4ef63c0
Lucas Cherkewski <lucas@lucascherkewski.com>
2024-08-26 19:51:08 -0400

    update the OCT database creation file to use legacy GTFS

--- processing/oct-gtfs-to-parquet.sql
+++ processing/oct-gtfs-to-parquet.sql
@@ -1,25 +1,25 @@
--- CREATE TABLE feed_info as SELECT * FROM read_csv("data/source/octranspo-gtfs/2024-08-24-GTFSExport/feed_info.txt", dateformat='%Y%m%d', types={'feed_start_date': 'DATE', 'feed_end_date': 'DATE'});
--- CREATE TABLE calendar as SELECT * FROM read_csv("data/source/octranspo-gtfs/2024-08-24-GTFSExport/calendar.txt", dateformat='%Y%m%d', types={'start_date': 'DATE', 'end_date': 'DATE'});
--- CREATE TABLE calendar_dates as SELECT * FROM read_csv("data/source/octranspo-gtfs/2024-08-24-GTFSExport/calendar_dates.txt", dateformat='%Y%m%d', types={'date': 'DATE'});
+-- CREATE TABLE feed_info as SELECT * FROM read_csv("data/source/octranspo-gtfs/2024-08-26-GTFSExport/feed_info.txt", dateformat='%Y%m%d', types={'feed_start_date': 'DATE', 'feed_end_date': 'DATE'});
+-- CREATE TABLE calendar as SELECT * FROM read_csv("data/source/octranspo-legacy-gtfs/2024-08-26-google_transit/calendar.txt", dateformat='%Y%m%d', types={'start_date': 'DATE', 'end_date': 'DATE'});
+-- CREATE TABLE calendar_dates as SELECT * FROM read_csv("data/source/octranspo-legacy-gtfs/2024-08-26-google_transit/calendar_dates.txt", dateformat='%Y%m%d', types={'date': 'DATE'});
 
 CREATE TABLE stops as
-	SELECT stop_id, stop_code, stop_name, stop_lat, stop_lon, platform_code
-	FROM read_csv("data/source/octranspo-gtfs/2024-08-24-GTFSExport/stops.txt");
+	SELECT stop_id, stop_code, stop_name, stop_lat, stop_lon
+	FROM read_csv("data/source/octranspo-legacy-gtfs/2024-08-26-google_transit/stops.txt");
 CREATE TABLE stop_times as
 	SELECT trip_id, arrival_time, stop_id, stop_sequence
-	FROM read_csv("data/source/octranspo-gtfs/2024-08-24-GTFSExport/stop_times.txt", types={'trip_id': 'VARCHAR', 'stop_id': 'VARCHAR'});
+	FROM read_csv("data/source/octranspo-legacy-gtfs/2024-08-26-google_transit/stop_times.txt", types={'trip_id': 'VARCHAR', 'stop_id': 'VARCHAR'});
 CREATE TABLE trips as
 	SELECT route_id, service_id, trip_id, trip_headsign, direction_id -- trip_headsign probably not useful? but maybe if we're trying to join / infer directions later
-	FROM read_csv("data/source/octranspo-gtfs/2024-08-24-GTFSExport/trips.txt");
+	FROM read_csv("data/source/octranspo-legacy-gtfs/2024-08-26-google_transit/trips.txt");
 
 -- add OCT GTFS as source
-ALTER TABLE stops ADD COLUMN source VARCHAR DEFAULT 'octranspo-gtfs';
+ALTER TABLE stops ADD COLUMN source VARCHAR DEFAULT 'octranspo-legacy-gtfs';
 ALTER TABLE stops ALTER COLUMN source DROP DEFAULT;
 
-ALTER TABLE stop_times ADD COLUMN source VARCHAR DEFAULT 'octranspo-gtfs';
+ALTER TABLE stop_times ADD COLUMN source VARCHAR DEFAULT 'octranspo-legacy-gtfs';
 ALTER TABLE stop_times ALTER COLUMN source DROP DEFAULT;
 
-ALTER TABLE trips ADD COLUMN source VARCHAR DEFAULT 'octranspo-gtfs';
+ALTER TABLE trips ADD COLUMN source VARCHAR DEFAULT 'octranspo-legacy-gtfs';
 ALTER TABLE trips ALTER COLUMN source DROP DEFAULT;
 
 -- load the NWTB data
@@ -96,7 +96,7 @@ UPDATE stop_times
 --- JSON generated with: https://observablehq.com/d/fb22d192264eb8f6
 CREATE TEMP TABLE gtfs_representative_services AS
     SELECT *
-    FROM read_csv("data/source/octranspo-gtfs/2024-08-24-gtfs_representative_services.csv");
+    FROM read_csv("data/source/octranspo-legacy-gtfs/2024-08-26-legacy-gtfs_representative_services.csv");
 
 UPDATE gtfs_representative_services
 	SET day_of_week = 'weekday'
