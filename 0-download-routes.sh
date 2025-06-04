1be7804c2f24beb2547b01993288039fb48a9b9b
Lucas Cherkewski <lucas@lucascherkewski.com>
2025-06-03 20:50:13 -0400

    significant cleanup of old JS approach

--- 0-download-routes.sh
+++
@@ -1,5 +0,0 @@
-curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-15' | jq > data/routes/weekday.json
-
-curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-13' | jq > data/routes/saturday.json
-
-curl 'https://plan.octranspo.com/plan/api/RouteTimetableAPI/GetRouteDirections?date=2025-9-14' | jq > data/routes/sunday.json
